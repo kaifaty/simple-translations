52e6c148f9812fcaf1d78c246aba9b879986edce
Kaifat <andry.diego2011@gmail.com>
2021-02-09 21:19:15 +0500

    up

--- translations.ts
+++ translations.ts
@@ -63,6 +63,38 @@ export const translate = {
         "en": "Confirmations",
         "ru": "Подтверждений"
     },
+    "privateKeyAttention": {
+        "en": "This is not a recommended way to access your wallet. Due to confidential information, these options should only be used in clean system and browser without any extensions by experienced users.",
+        "ru": "Это нерекомендуемый способ доступа к вашему кошельку. Из-за конфиденциальной информации эти параметры должны использоваться только в чистой системе и браузере без каких-либо расширений опытными пользователями."
+    },
+    "privateKey": {
+        "en": "Private key",
+        "ru": "Приватный ключ"
+    },
+    "privateKey_placeholder": {
+        "en": "Enter your private key",
+        "ru": "Введите ваш приватный ключ"
+    },
+    "button_save": {
+        "en": "Save",
+        "ru": "Сохранить"
+    },
+    "keystore": {
+        "en": "Keystore file",
+        "ru": "Файл кeystore"
+    },
+    "password": {
+        "en": "Password",
+        "ru": "Пароль"
+    },
+    "password_placeholder": {
+        "en": "Enter password",
+        "ru": "Введите пароль"
+    },
+    "choseFile": {
+        "en": "Choose file",
+        "ru": "Выбрать файл"
+    },
     get(key: string, lang: string, values? : {[key: string]: string}){
         let res = this[key]?.[lang] || this[key]?.en || key
         if(values){
